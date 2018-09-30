051c47e409e0c8b6af9f6da8311acf716ede1600
TharinduMPerera <tharindu.madushanka@my.sliit.lk>
2018-09-30 23:07:29 +0530

    set navigation bar preference

--- iTunes-Search/iTunes-Search/Controllers/DetailsViewController.swift
+++ iTunes-Search/iTunes-Search/Controllers/DetailsViewController.swift
@@ -9,9 +9,19 @@
 import UIKit
 
 class DetailsViewController: UIViewController {
+    
+    var application : Application?
 
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.navigationBar.prefersLargeTitles = false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
 
 }
