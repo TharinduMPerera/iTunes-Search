f275131f0ce3b7b7c63626201b2956c103c09912
TharinduMPerera <tharindu.madushanka@my.sliit.lk>
2018-09-30 11:12:37 +0530

    added Application struct

---
+++ iTunes-Search/iTunes-Search/Models/Application.swift
@@ -0,0 +1,23 @@
+//
+//  Application.swift
+//  iTunes-Search
+//
+//  Created by Tharindu Perera on 9/30/18.
+//  Copyright © 2018 Tharindu Perera. All rights reserved.
+//
+
+import Foundation
+
+struct Application {
+    let trackName : String
+    let sellerName : String
+    let version : String
+    let description : String
+    let wrapperType : String
+    let primaryGenreName : String
+    let formattedPrice : String
+    let artworkUrl100 : String
+    let fileSizeBytes : String
+    let averageUserRating : String
+    let contentAdvisoryRating : String
+}
