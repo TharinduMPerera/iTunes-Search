d5436b5bfd0ab196a3af58f35396d99a7cc73d05
TharinduMPerera <tharindu.madushanka@my.sliit.lk>
2018-09-30 22:49:14 +0530

    init DetailsViewController

---
+++ iTunes-Search/iTunes-Search/Controllers/DetailsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailsViewController.swift
+//  iTunes-Search
+//
+//  Created by Tharindu Perera on 9/30/18.
+//  Copyright © 2018 Tharindu Perera. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
